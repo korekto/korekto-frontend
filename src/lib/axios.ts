56a84238fb87c48f368d6b7ce631a45f0b4ffa0c
ledoyen <ledoyen.loic@gmail.com>
2024-04-25 12:19:52 +0200

    Upgrade dependencies versions to latest possible

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -19,13 +19,13 @@ axiosAPI.interceptors.response.use(
 
 const isoDateFormat = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(?:\.\d*)?(?:[-+]\d{2}:?\d{2}|Z)?$/;
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 function isIsoDateString(value: any): boolean {
-	// eslint-disable-line @typescript-eslint/no-explicit-any
 	return value && typeof value === 'string' && isoDateFormat.test(value);
 }
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function handleDates(body: any) {
-	// eslint-disable-line @typescript-eslint/no-explicit-any
 	if (body === null || body === undefined || typeof body !== 'object') return body;
 
 	for (const key of Object.keys(body)) {
